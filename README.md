2f65ed005d71d47a502d0c825995bc3de69cbf60
Winky <whita_mac@Mac.lan>
2025-03-29 09:16:53 -0500

    added screen capture updated readme to start work on handling platform differences

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This is a simple Expo app for me to play around with and test ideas.
 ## 
 
 Following this tutorial: Expo tutorial[https://docs.expo.dev/tutorial/build-a-screen/]
- - On step 7 out of 9 (Take a screenshot)
+ - On step 8 out of 9 (Handle Platform Differences)
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
 import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState, useRef } from 'react';
+import { captureRef } from 'react-native-view-shot';
+import { type ImageSource } from 'expo-image';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as MediaLibrary from 'expo-media-library';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -8,19 +14,20 @@ import EmojiPicker from '@/components/EmojiPicker';
 import EmojiList from '@/components/EmojiList';
 import EmojiSticker from '@/components/EmojiSticker';
 
-import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-import { type ImageSource } from 'expo-image';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-
+  
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    requestPermission();
+  }
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -50,14 +57,28 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert('Saved!');
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
